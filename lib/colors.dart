e7fc8b7f784d9524aca5d62a0236fb8829dfb86d
VITALI-ASUS\Asus <vitalibof@gmail.com>
2018-10-17 16:08:26 +0300

    Splash screen added

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color colorPrimary = const Color(0xFFF0F0F0);
+Color colorPrimaryDark = const Color(0xFF8F9199);
+Color colorAccent = const Color(0xFFF57524);
+Color colorButtonEnable = colorAccent;
+Color colorButtonDisable = const Color(0xFF96f57523);
