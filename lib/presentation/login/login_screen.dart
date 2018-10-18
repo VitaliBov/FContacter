025aaf12b64ab2bf2e94643908b91dba0eb3943e
VITALI-ASUS\Asus <vitalibof@gmail.com>
2018-10-18 13:37:30 +0300

    Login WebView created

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -1,14 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class LoginScreen extends StatelessWidget {
-  LoginScreen({Key key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  static const redirectUrl = "http://www.orangesoft.by/";
+  static const query = "code";
+  static const url = "https://www.teamwork.com/launchpad/login?redirect_uri=$redirectUrl";
+  final webViewPlugin = new FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
+  String code;
+
+  @override
+  void initState() {
+    super.initState();
+    webViewPlugin.close();
+    _onUrlChanged = webViewPlugin.onUrlChanged.listen((String url) {
+      if (mounted) {
+        setState(() {
+          if (url.startsWith(redirectUrl)) {
+            Uri uri = Uri.parse(url);
+            if (uri.hasQuery) {
+              code = uri.queryParameters[query];
+
+              webViewPlugin.close();
+            }
+          }
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-
-        )
+    return new WebviewScaffold(
+      url: url,
     );
+  }
+
+  @override
+  void dispose() {
+    _onUrlChanged.cancel();
+    webViewPlugin.dispose();
+    super.dispose();
   }
 }
